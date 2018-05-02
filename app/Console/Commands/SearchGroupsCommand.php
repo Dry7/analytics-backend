c1de6c371156cfcc7b13567ac0bed2a78a8da99a
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-03 01:42:04 +0300

    Search groups command

---
+++ app/Console/Commands/SearchGroupsCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\UpdateGroupJob;
+use App\Types\Network;
+use Illuminate\Console\Command;
+
+class SearchGroupsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'analytics:search-groups';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Search new groups';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        for ($i = 817; $i <= 1000; $i++) {
+            echo "\n" . $i;
+            UpdateGroupJob::dispatch(Network::VKONTAKTE, 'club' . $i)->onQueue('vk:search-groups');
+        }
+    }
+}
