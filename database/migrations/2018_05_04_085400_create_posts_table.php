426e5e19b891c90bf19aeec344895e44e476e5eb
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-04 13:27:49 +0300

    Posts migration

---
+++ database/migrations/2018_05_04_085400_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('group_id')->comment = 'ID группы в аналитике';
+            $table->unsignedBigInteger('post_id')->comment = 'ID поста в социальной сети';
+            $table->dateTime('date')->comment = 'Дата поста';
+            $table->unsignedInteger('likes')->nullable()->comment = 'Актуальное количество лайков';
+            $table->unsignedInteger('shares')->nullable()->comment = 'Актуальное количество репостов';
+            $table->unsignedInteger('views')->nullable()->comment = 'Актуальное количество просмотров';
+            $table->unsignedInteger('comments')->nullable()->comments = 'Актуальное количество комментариев';
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
