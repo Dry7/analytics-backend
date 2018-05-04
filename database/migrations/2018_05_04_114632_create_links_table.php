916f6768b4e573fd54d75973509e512e6a84c7de
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-04 14:57:58 +0300

    Links migration

---
+++ database/migrations/2018_05_04_114632_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('group_id')->comment = 'ID группы в аналитике';
+            $table->unsignedBigInteger('post_id')->comment = 'ID поста в социальной сети';
+            $table->string('url')->comment = 'Ссылка';
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
