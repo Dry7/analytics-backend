4fc01e3de183d00a047b37115edaf7ad3de0a97d
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-05 18:19:23 +0300

    Group history model

---
+++ app/Models/GroupsHistory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GroupsHistory extends Model
+{
+    protected $fillable = [
+        'group_id', 'date', 'members', 'members_possible', 'posts', 'likes', 'avg_posts', 'avg_comments_per_post',
+        'avg_likes_per_post', 'avg_shares_per_post', 'avg_views_per_post'
+    ];
+}
