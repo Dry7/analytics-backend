bbf8219aa70f78a26bedfb51b422689fd63c330f
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-09 19:30:35 +0300

    Influx singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Services\CountryService;
+use App\Services\InfluxService;
 use Illuminate\Support\ServiceProvider;
+use InfluxDB\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,20 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(CountryService::class);
+        $this->app->singleton(Client::class, function () {
+            return new Client(
+                config('services.influx.host'),
+                config('services.influx.port'),
+                config('services.influx.username'),
+                config('services.influx.password')
+            );
+        });
+        $this->app->singleton(InfluxService::class, function () {
+            $service = app(Client::class);
+            return new InfluxService(
+                $service,
+                $service->selectDB(config('services.influx.database'))
+            );
+        });
     }
 }
