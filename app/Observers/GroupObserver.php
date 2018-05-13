4fd8cedd3e141a13106b8a85a8c7b15deb12c99c
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-13 19:26:36 +0300

    GroupObserver

---
+++ app/Observers/GroupObserver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Group;
+use App\Services\ElasticSearchService;
+
+class GroupObserver
+{
+    /** @var ElasticSearchService */
+    private $elasticSearch;
+
+    /**
+     * GroupObserver constructor.
+     * @param ElasticSearchService $elasticSearchService
+     */
+    public function __construct(ElasticSearchService $elasticSearchService)
+    {
+        $this->elasticSearch = $elasticSearchService;
+    }
+
+    /**
+     * @param Group $group
+     */
+    public function saved(Group $group)
+    {
+        $this->elasticSearch->index($group);
+    }
+
+    /**
+     * @param Group $group
+     */
+    public function deleting(Group $group)
+    {
+        $this->elasticSearch->delete($group);
+    }
+}
