f827f7c3c4cf0b94e3a14268c989d9f91adafeb7
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-13 19:26:23 +0300

    Add middleware to register

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register', 'ApiController@register');
-Route::post('/touch', 'ApiController@touch');
+
+Route::middleware(['auth.api'])->group(function () {
+    Route::post('/register', 'ApiController@register');
+    Route::post('/touch', 'ApiController@touch');
+});
