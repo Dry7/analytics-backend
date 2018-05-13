2a7bf5c4cfb7c31c27801010dbadda3695098e80
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-13 19:22:58 +0300

    AuthApi

---
+++ app/Http/Middleware/ApiAuth.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+
+class ApiAuth
+{
+    private const X_API_KEY = 'X-API-KEY';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws Exception
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->header(self::X_API_KEY, null) === 'test') {
+            return 'Invalid API key';
+        }
+
+        return $next($request);
+    }
+}
