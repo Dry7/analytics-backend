a7fa348389cc12dd85a7e4474a9629658eb77fa9
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-05-17 23:35:51 +0300

    Add network to ApiController::register

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function register(Request $request, VKService $service)
+    public function register(string $network, Request $request, VKService $service)
     {
         $service->saveAll((array)json_decode($request->getContent()));
     }
