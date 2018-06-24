71c0ea497c3048723f188816b4edab3500b362aa
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-06-24 21:47:23 +0300

    Link resource

---
+++ app/Http/Resources/LinkResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class LinkResource
+ *
+ * @package App\Http\Resources
+ */
+class LinkResource extends JsonResource
+{
+    public static $wrap = null;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'groupId' => $this->resource->group_id,
+            'url' => $this->resource->url,
+            'isAd' => $this->resource->is_ad,
+            'post' => new PostResource($this->resource->post),
+        ];
+    }
+
+    public function withResponse($request, $response)
+    {
+        $response->header('Access-Control-Allow-Origin', '*');
+    }
+}
