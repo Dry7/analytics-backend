fd4328b2b28141fa7f41591b20fc253e912500ad
Dry7 <andrey.dry7.shilov@yandex.ru>
2018-10-12 12:14:28 +0300

    Change partition config

--- config/analytics/partitions.php
+++ config/analytics/partitions.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'rows_in_partition' => env('PARTITION_ROWS_IN_PARTITION', 10),
-    'min_free_space_partition' => env('MIN_FREE_SPACE_IN_PARTITION', 5),
+    'rows_in_partition' => env('PARTITION_ROWS_IN_PARTITION', 1000000),
+    'min_free_space_partition' => env('MIN_FREE_SPACE_IN_PARTITION', 50000),
 ];
