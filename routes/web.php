4be4809fd0db255276dc6f00e44cb44382e84289
Dry7 <andrey.dry7.shilov@yandex.ru>
2019-01-25 22:42:08 +0300

    refactoring web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
-Route::post('/api/groups', 'GroupController@groups');
-
 Route::middleware([\App\Http\Middleware\AccessControl::class])->group(function () {
 
+    Route::post('/api/groups', 'GroupController@groups');
     Route::get('/api/groups/{group}', 'GroupController@group');
     Route::get('/api/groups/{group}/links', 'GroupController@links');
     Route::get('/api/groups/{group}/statistics', 'GroupController@statistics');
+    Route::get('/api/dictionary/groups', 'GroupController@groupsShort');
 
     Route::post('/api/ads', 'AdController@ads');
 
-    Route::get('/api/dictionary/groups', 'GroupController@groupsShort');
+    Route::get('/api/countries', 'CountryController@countries');
+    Route::get('/api/countries/{countryCode}/states', 'CountryController@states');
+    Route::get('/api/countries/{countryCode}/states/{stateCode}/cities', 'CountryController@cities');
 });
 
-Route::get('/api/countries', 'CountryController@countries');
-Route::get('/api/countries/{countryCode}/states', 'CountryController@states');
-Route::get('/api/countries/{countryCode}/states/{stateCode}/cities', 'CountryController@cities');
 
 Route::get('/test/phpinfo', function () {
     phpinfo();
